77d08f272ffeb1a8ee58ff11b2483c944a439f4a
Addy Osmani <addyosmani@gmail.com>
2016-11-30 17:12:59 -0800

    src/App.vue: reference Todos components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <Todos></Todos>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Todos from './components/Todos'
 
 export default {
-  name: 'app',
-  components: {
-    Hello
-  }
+    name: 'app',
+    components: {
+        Todos
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

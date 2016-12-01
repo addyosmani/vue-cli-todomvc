3a6a0ae1714bdddbc57a316a09e5640b6d228d71
Addy Osmani <addyosmani@gmail.com>
2016-11-30 17:17:17 -0800

    Add main.js source with very basic routing.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,42 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+window.VueApp = new Vue({
+    el: '#app',
+    render: h => h(App)
 })
+
+
+// visibility filters
+var filters = {
+  all: function (todos) {
+    return todos
+  },
+  active: function (todos) {
+    return todos.filter(function (todo) {
+      return !todo.completed
+    })
+  },
+  completed: function (todos) {
+    return todos.filter(function (todo) {
+      return todo.completed
+    })
+  }
+}
+
+// handle routing
+function onHashChange () {
+  var visibility = window.location.hash.replace(/#\/?/, '')
+  if (filters[visibility]) {
+    window.VueApp.filter = visibility;
+  } else {
+    window.location.hash = ''
+    window.VueApp.filter = 'all'
+  }
+}
+
+window.addEventListener('hashchange', onHashChange)
+onHashChange()
